7868f373e4a423d34ebc821b345852f08231376c
kurihiro0119 <54324711+kurihiro0119@users.noreply.github.com>
2020-02-11 03:57:07 +0900

    class化

--- send_message.py
+++ send_message.py
@@ -2,24 +2,30 @@ from email.mime.text import MIMEText
 from email.utils import formatdate
 import smtplib
 
-fromaddress = 'sample@gmail.com'
-toaddress = 'sample@hotmail.co.jp'
-password = 'password'
+class Send_Message:
+    fromaddress = 'sample@gmail.com'
+    toaddress = 'sample@hotmail.co.jp'
+    password = 'password'
 
-smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpobj.ehlo()
-smtpobj.starttls()
-smtpobj.ehlo()
+#初期化
+    def __init__(self, fromaddress, toaddress, password):
+        self.fromaddress = fromaddress
+        self.toaddress = toaddress
+        self.password = password
 
-#メールアドレスとパスワードを変更してください。
-smtpobj.login(fromaddress, password)
 
-msg = MIMEText('body message')
-msg['Subject'] = 'subject'
-msg['From'] = fromaddress
-msg['To'] = toaddress
-msg['Date'] = formatdate()
-print(msg)
+    def send(self, content):
+        smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+        smtpobj.ehlo()
+        smtpobj.starttls()
+        smtpobj.ehlo()
+        smtpobj.login(self.fromaddress, self.password)
 
-smtpobj.sendmail(fromaddress, toaddress, msg.as_string())
-smtpobj.close()
+        msg = MIMEText(content)
+        msg['Subject'] = 'subject'
+        msg['From'] = self.fromaddress
+        msg['To'] = self.toaddress
+        msg['Date'] = formatdate()
+
+        smtpobj.sendmail(self.fromaddress, self.toaddress, msg.as_string())
+        smtpobj.close()
